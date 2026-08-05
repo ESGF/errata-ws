08afb385dbe100d14f2e3ff030b88f1a1a088844
Compte_de_Service ESPRI-MOD <esprimod@esgf-errata.private.ipsl.fr>
2026-08-05 16:10:11 +0200

    fix: Correct CORDEX-CMIP6 collections

--- resources/mappers.py
+++ resources/mappers.py
@@ -36,7 +36,7 @@ metadata_mapper = {
                 'key': 'cordex-cmip6',
                 'label': 'CORDEX-CMIP6',
                 'namespace': 'cordex-cmip6',
-                'facets': ['institute', 'experiment', 'rcm-model', 'variable'],
+                'facets': ['institution_id', 'driving_experiment_id', 'source_id', 'variable_id'],
                 'is_documented': False,
                 'is_pid_client': False
             },
